c96ede9a657b104b092be92b84a7c85dafecae5e
Qingyun Wang <lucas.qingyun@gmail.com>
2023-12-08 01:46:21 +0200

    Add plane image

--- update.sql
+++ update.sql
@@ -17,3 +17,27 @@ create table tasks (
     foreign key (departure_id) references airport(id),
     foreign key (destination_id) references airport(id)
 );
+
+update aircraft
+set aircraft.image = '\static\imgs\flight_1'
+where id = 1;
+update aircraft
+set aircraft.image = '\static\imgs\flight_2'
+where id = 2;
+update aircraft
+set aircraft.image = '\static\imgs\flight_3'
+where id = 3;
+update aircraft
+set aircraft.image = '\static\imgs\flight_4'
+where id = 4;
+update aircraft
+set aircraft.image = '\static\imgs\flight_5'
+where id = 5;
+update aircraft
+set aircraft.image = '\static\imgs\flight_6'
+where id = 6;
+update aircraft
+set aircraft.image = '\static\imgs\flight_7'
+where id = 7;
+alter table aircraft
+modify column image varchar(128);
